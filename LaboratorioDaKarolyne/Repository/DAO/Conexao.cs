456a23420f97327071b1cf3bf399857c7630c703
Karolyne Costa <karolyne.costa@infouniron.com>
2017-10-09 23:00:11 -0300

    String de Conexão adicionado

---
+++ LaboratorioDaKarolyne/Repository/DAO/Conexao.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace LaboratorioDaKarolyne.Repository.DAO
+{
+    public class Conexao
+    {
+        private SqlConnection Conectar()
+        {
+            //Para poder ter acesso ao BD
+            string stringConexao = ConfigurationManager.ConnectionStrings["ConexaoLaboratorio"].ConnectionString;
+            //Conectar o BD
+            SqlConnection conexao = new SqlConnection(stringConexao);
+            //Abrir o BD
+            conexao.Open();
+
+            return conexao;
+        }
+
+        public int ExecutarCrud(SqlCommand comando)
+        {
+            SqlConnection con = Conectar();
+            comando.Connection = con;
+            int id = (int)comando.ExecuteScalar();
+            con.Close();
+
+            return id;
+            //Toda vez que o comando ExecutarCrud for chamado ele se conectará ao banco para ter acesso as informações do banco, podendo assim executar o update, delete, insert e busca.            
+        }
+
+        public SqlDataReader ExecutarSelect(SqlCommand comando)
+        {
+            SqlConnection con = Conectar();
+            comando.Connection = con;
+            SqlDataReader dr = comando.ExecuteReader(CommandBehavior.CloseConnection);
+
+            return dr;
+            //É com o SqlDataReader que vamos fazer o select no banco para podermos fazer uma busca
+        }
+    }
+}
