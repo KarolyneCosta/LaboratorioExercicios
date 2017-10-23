544786670f0fdb5e74f1242a31c157a04b92412f
Karolyne Costa <karolyne.costa@infouniron.com>
2017-10-22 23:59:33 -0300

    CidadeController Quase Pronta

--- LaboratorioDaKarolyne/Controllers/CidadeController.cs
+++ LaboratorioDaKarolyne/Controllers/CidadeController.cs
@@ -15,5 +15,51 @@ namespace LaboratorioDaKarolyne.Controllers
             IList<Cidade> listaDeCidades = new Cidade().GetAll();
             return View(listaDeCidades);
         }
+
+        public ActionResult Novo()
+        {
+            Cidade c = new Cidade();
+            return View(c);
+        }
+
+        [HttpPost]
+        public ActionResult Novo(Cidade c)
+        {
+            if (ModelState.IsValid)
+            {
+                c.Save();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(c);
+            }
+        }
+
+        public ActionResult Excluir(int id)
+        {
+            Cidade objCidade = new Cidade().FindById(id);
+            return View(objCidade);
+        }
+
+        [HttpPost]
+        public ActionResult Excluir(Cidade objCidade)
+        {
+            objCidade.Deletar(objCidade.IdCidade);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Editar(int id)
+        {
+            Cidade objCidade = new Cidade().FindById(id);
+            return View(objCidade);
+        }
+
+        [HttpPost]
+        public ActionResult Editar(Cidade objCidade)
+        {
+            objCidade.Save();
+            return RedirectToAction("Index");
+        }
     }
 }
